cd67a70e48b5771dbf32476a91b0210d4b225d1a
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-08-04 11:56:24 +0900

    코드

---
+++ lunch.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import requests
+from datetime import datetime
+import pytz
+import pandas as pd
+from streamlit.components.v1 import html
+def get_posts(id):
+    url = f"https://pf-wapi.kakao.com/web/profiles/{id}/posts"
+    req = requests.get(url)
+    json = req.json()
+    return json
+def get_media(id, post_id):
+    url = f"https://pf-wapi.kakao.com/web/profiles/{id}/posts/{post_id}"
+    req = requests.get(url)
+    json = req.json()
+    media = json["media"]
+    return media
+def show_menus(store_id):
+    posts = get_posts(store_id)
+    post_ids = [p['id'] for p in posts['items']]
+    images = []
+    for post_id in post_ids:
+        media = get_media(store_id, post_id)
+        for m in media:
+            images.append(m['xlarge_url'])
+    cols = st.columns(min(3, len(images)))
+    for i, col in enumerate(cols[:3]):
+        col.image(images[i])
+st.set_page_config(
+    page_title="구내식당 갈까?",
+    page_icon=":나이프_포크_접시:",
+    # layout="wide",
+)
+st.title("6300원의 행복")
+now = datetime.now(pytz.timezone("Asia/Seoul"))
+st.metric(
+    label="현재일자",
+    value=now.strftime("%Y-%m-%d"))
+st.write("## The 좋은밥상")
+# df = pd.DataFrame(
+#     [[37.4682657, 126.886182]],
+#     columns=['lat', 'lon'])
+# st.map(df, zoom=14)
+show_menus("_xfWxfCxj")
+# st.write("## 우림구내식당")
+# # df = pd.DataFrame(
+# #     [[37.4668171, 126.888310]],
+# #     columns=['lat', 'lon'])
+# # st.map(df, zoom=14)
+# show_menus("_ixcNxexj")
+
+
+
+
+
+
+
+
+
+
+
+Sociopath에 메시지 보내기
+
+
+
+
+
+
+
+
+
